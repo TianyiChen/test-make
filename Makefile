55cefe28ede5c7858bee4cf5c71b02c8ed7fec45
Tianyi Chen <TianyiChen@users.noreply.github.com>
2023-01-23 08:03:11 -0800

    json

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-CPP= $(CXX) -std=c++20 -O2
+CPP= $(CXX) -fsanitize=address -std=c++20 -O2
 test:
 	$(CPP) hello.cpp -o hello
 	./hello
--- hello.cpp
+++ hello.cpp
@@ -1,4 +1,29 @@
 #include<bits/stdc++.h>
+#include"json.hpp"
 using namespace std;
+template <class T>
+struct TestAlloc {
+	T *allocate(size_t n) {
+		return new T[n];
+	}
+	void deallocate(T *p, std::size_t n) {
+		std::cerr << "delete " << (void *)p << ' ' << n << '\n';
+		delete[] p;
+	}
+	using value_type = T;
+	using size_type = size_t;
+	using difference_type = ptrdiff_t;
+};
+template <class T>
+using Alloc = TestAlloc<T>;
 int main(){
+	using J = nlohmann::basic_json<std::map, std::vector, basic_string<char, char_traits<char>, Alloc<char>>, bool, int64_t, uint64_t, double, Alloc>;
+	auto j = J ::parse(R"(
+	 {
+    "happy": true,
+    "pi": 3.141,
+	"hello": "world"
+ 	 }
+	)");
+	cout<<j["hello"]<<endl;
 }
